af43f2de01bbd6d48c2bd4f2c287ad64d01b1e0b
C Selvaraju <trainerselva@gmail.com>
2024-02-15 13:22:43 +0530

    Initial Commit

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def landing_page(request):
+    return render(request, "blog/index.html")
+
+def posts(request):
+    return render(request, "blog/all-posts.html")
+
+def single_post(request):
+    pass
+
