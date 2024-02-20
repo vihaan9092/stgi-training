19175a1d90843dda9ff99563fb045e8af24f439b
Vihaan singla <vsingla@thesummitgrp.com>
2024-02-20 17:46:24 +0530

    initial commit

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,24 @@
+import json
 from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+POSTS_FILE = 'blog/data/posts.json'
+
+def load_posts():
+    with open(POSTS_FILE, 'r') as file:
+        posts = json.load(file)
+    return posts
 
 def landing_page(request):
-    return render(request, "blog/index.html")
+    return render(request, 'blog/index.html')
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    posts = load_posts()
+    return render(request, 'blog/all-posts.html', {'posts': posts})
 
-def single_post(request):
-    pass
-
+def single_post(request, slug):
+    posts = load_posts()
+    post = next((post for post in posts if post['id'] == slug), None)
+    if post is None:
+        raise Http404('Post not found')
+    return render(request, 'blog/single-post.html', {'post': post})
